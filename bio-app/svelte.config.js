a4c05dee95b8b026dd420bda72d7a206cffbb861
mihainsto <mihaistoianro@gmail.com>
2024-12-31 13:49:18 +0200

    fix

--- bio-app/svelte.config.js
+++ bio-app/svelte.config.js
@@ -1,17 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-const dev = process.env.NODE_ENV === 'development';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter(),
-    paths: {
-      base: dev ? '' : '/bio'
-    }
-  },
-  preprocess: vitePreprocess()
-};
-
-export default config;
+export default {
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+}
